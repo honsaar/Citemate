a551fec21ea67c88447ca5c695e85d58e61c0394
Aaron Honson <aaron.shannon-honson@adelaide.edu.au>
2019-08-20 12:46:49 +0930

    Structuring page layout and loose styles
    Feeling cute, might rebase later idk

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,76 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+      <b-navbar id="nav" toggleable="lg">
+      <!-- logo -->
+      <b-navbar-brand class="brand">Citemate.</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+       <b-collapse id="nav-collapse" is-nav>
+      <!-- nav links, pull right -->
+       <b-navbar-nav class="ml-auto">
+        <b-nav-item> <router-link to="/">Home </router-link> </b-nav-item>
+        <b-nav-item> <router-link to="/about">About </router-link></b-nav-item>
+        <b-nav-item> <router-link to="/citations">My Citations </router-link></b-nav-item>
+
+        <!-- <b-nav-item> <router-link to="/about"><b-btn>Get Started</b-btn></router-link></b-nav-item> -->
+       </b-navbar-nav>
+       </b-collapse>
+      </b-navbar>
+
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url('https://rsms.me/inter/inter.css');
+@font-face {
+    font-family: Bagnard;
+    src: url("./assets/Bagnard.otf") format("opentype");
+}
+
+html {
+
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Inter', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #273043;
+  background: #F5F6F3;
 }
+
+@supports (font-variation-settings: normal) {
+  #app { font-family: 'Inter var', sans-serif; }
+}
+
 #nav {
   padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  color: #273043;
+  padding-right: 1em;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #ff8552;
+}
+
+.right {
+  float: right;
+}
+
+.left {
+    float: left;
+}
+
+.brand {
+  font-family: 'Bagnard', serif;
+}
+
+.navbar-toggler {
+  /* override Bootstrap styling of toggler */
+  border: none !important; 
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,134 @@
 <template>
   <div class="home">
-    <div class="container">
-    <p>AAA</p>
-    <b-btn variant="primary" @click="cite()">Cite Journal</b-btn>
-    <br/><br/>
-    <div class="results">
-      <div v-for="(paper, key) in results" :key="key">
-        <h2><strong>Title:</strong> {{paper.title}}</h2>
-        <p><strong>Authors:</strong> {{paper.authors}}</p>
-        <p><strong>Published By:</strong> {{paper.publisher}}</p>
-        <p><strong>Appeared In:</strong> {{paper.journal}}</p>
+    <!-- hero/cta biz -->
+    <div class="hero">
+      <div class="container">
+        <h1 class="brand">Citations simplified.</h1>
+        <p class="subtitle">
+          Stop doing your references by hand. Create citations, reference lists and bibliographies automatically and
+          <span
+            class="tagline"
+          >focus on what matters</span>.
+        </p>
         <br/>
+        <!-- TODO: Too similar to Citationsy, change it up. The irony! -->
+         <b-btn variant="primary" class="primeButt">Create a Bibliography</b-btn>
       </div>
     </div>
+
+    <div class="content" style="background: white;">
+      <div class="container">
+        <div id="features">
+          <b-row>
+            <b-col>
+              <p><img src="../assets/ref.svg"/></p>
+              <p class=""><strong>Your format, your way</strong></p>
+              <p>Format your references for Harvard, MLA, Chicago styles plus many others</p>
+            </b-col>
+            <b-col>
+              <p><img src="../assets/priv.svg"/></p>
+              <p class=""><strong>Truly private</strong></p>
+              <p>Your references are yours, and yours alone</p>
+            </b-col>
+            <b-col>
+              <p><img src="../assets/bibl.svg"/></p>
+              <p class=""><strong>Robust and ready</strong></p>
+              <p>Build your bibliography and reference in your text</p>
+            </b-col>
+          </b-row>
+        </div>
+          <br><br><br>
+        <p style="text-align: center;"><b-btn variant="primary" class="primeButt" @click="cite()">Test cite, yo</b-btn></p>
+        <br />
+        <br />
+        <div class="results">
+          <div class="resultCard" v-for="(paper, key) in results" :key="key">
+            <p>
+              <strong>{{paper.title}}</strong>
+            </p>
+            <p>
+              <span v-for="(author, index) in paper.authors" :key="index">
+                {{author.given}} {{author.family}}
+                <span v-if="index < paper.authors.length-1">,</span>
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.hero {
+  text-align: center;
+  min-height: 400px;
+  padding-top: 10em;
+  background: #f5f6f3;
+}
+.resultCard {
+  background: white;
+  border-radius: 10px;
+  margin: 1em;
+  padding: 1em;
+}
+#features {
+  padding-top: 5em;
+  text-align: center;
+}
 
+#features .brand {
+  font-size: 1.3em;
+}
+
+#features img {
+  width: 40px;
+}
+
+ .primeButt {
+   background: #ff8552;
+
+   border: none;
+   border-radius: 0;
+ }
+</style>
 <script>
 // @ is an alias to /src
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-
-  },
-  data: function(){
+  name: "home",
+  components: {},
+  data: function() {
     return {
-    results: []
-    }
+      results: [],
+      query:
+        "Is+chronic+breathlessness+lessrecognised+and+treated+compared+with+chronic+pain"
+    };
   },
-methods: {
+  methods: {
     cite() {
       console.log("Loading response...");
-      axios.get("https://api.crossref.org/works?query=Is+chronic+breathlessness+lessrecognised+and+treated+compared+with+chronic+pain").then((response) => {
+      axios
+        .get("https://api.crossref.org/works?query=" + this.query)
+        .then(response => {
           console.log("Loaded!");
-          
-          
-          for(let i = 0; i < response.data.message.items.length; i++){
+          for (let i = 0; i < response.data.message.items.length; i++) {
             var search = response.data.message.items[i];
             var tempResult = {
-              title: '', //title[0]
-              doi: '', //DOI
+              title: "", //title[0]
+              doi: "", //DOI
               authors: [], //author[]
-              published: {year: '', month: '', day: ''}, //created
-              issue: '', //issue
-              publisher: '', //publisher
-              link: '', //url
-              volume: '', //volume
-              journal: '', //container-title[0]
-              pages: '' //page
-            }
-            console.log(search);
-            if(search.title != null || search.title != undefined){
-            tempResult.title = search.title[0];
+              published: { year: "", month: "", day: "" }, //created
+              issue: "", //issue
+              publisher: "", //publisher
+              link: "", //url
+              volume: "", //volume
+              journal: "", //container-title[0]
+              pages: "" //page
+            };
+
+            if (search.title != null || search.title != undefined) {
+              tempResult.title = search.title[0];
             }
             tempResult.doi = search.DOI;
             tempResult.authors = search.author;
@@ -67,17 +141,15 @@ methods: {
             tempResult.volume = search.volume;
             tempResult.journal = search["container-title"][0];
             tempResult.pages = search.page;
-            
-            console.log(tempResult);
             this.results.push(tempResult);
+            console.log(search);
           }
           console.log(this.results);
-          })
-      
+        });
     },
-    book(){
+    book() {
       console.log("Loading response for book libraries...");
     }
   }
-}
+};
 </script>
